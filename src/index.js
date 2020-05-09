1622ee516d4b1e6221d96f87209b49f936541f5c
zhangyuhui <842403078@qq.com>
2020-05-09 17:36:21 +0800

    区分浏览器端还是服务器端，增加判断

--- src/index.js
+++ src/index.js
@@ -15,27 +15,38 @@ class CrossIO {
    * @param  {Object} config 如果是http,和axios中config保持一致
    * @param  {Function} callback 调用方法传入的回调函数，参数为返回体(http请求时)
    * @param  {String} type http || pm http：使用http请求， pm: postMessage形式 不传默认走http
-  */
+   */
   sendMessage(config, callback, type) {
-    const sendType = judgeSendType(type)
-    const fn = sendWays[sendType] || loop
-    fn(config, callback || loop)
+    if (window) {
+      // 运行在浏览器端
+      const sendType = judgeSendType(type)
+      const fn = sendWays[sendType] || loop
+      fn(config, callback || loop)
+    } else {
+      // 运行在服务器端
+      
+    }
   }
 
   /**监听方法
    * @param {Object} config 必传url, 如要携带参数，使用params
    * @param  {Function} callback 监听到数据的时候的回调函数，参数为接收的数据
    * @param {String} type ws | lc | pm 可传入手动指定参数，分别代表websocket，长轮询，和postMessage,不传时优先判断是否可以使用websocket
-  */
+   */
   onMessage(config, callback, type) {
-    const connectType = judgeConnectType(type)
-    const fn = connectionWays[connectType].bind(this) || loop
-    fn(config, callback || loop)
+    if (window) {
+      // 运行在浏览器端
+      const connectType = judgeConnectType(type)
+      const fn = connectionWays[connectType].bind(this) || loop
+      fn(config, callback || loop)
+    } else {
+      // 运行在服务器端
+    }
   }
 
   /**
    * 断开方法
-  */
+   */
   close() {
     this.stopConnectionFn()
   }
